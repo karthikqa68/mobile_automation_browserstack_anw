82f56bcce5427d70645523351526293c7f1edc3f
arunkumar <arunkumar.nandigam@carbonplace.com>
2024-03-27 14:51:48 +0530

    url change

--- android/src/main/java/managers/AndroidWebDriverManager.java
+++ android/src/main/java/managers/AndroidWebDriverManager.java
@@ -88,7 +88,7 @@ public class AndroidWebDriverManager {
 		wait(15000);
 
 		try {
-			String gridUrl = "http://127.0.0.1:4444/wd/hub";
+			String gridUrl = "http://localhost:4444/wd/hub";
 			aDriver = new AndroidDriver(new URL(gridUrl), options);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
